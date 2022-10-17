8abcc74638accc5dca89a64707753b4d8e66073c
wlsgh7608 <62232531+wlsgh7608@users.noreply.github.com>
2022-10-17 21:18:35 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 652 ms, Memory: 133360 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int[] arr;
+	static List<Integer> list = new ArrayList<>();
+
+	static int getIdx(int n) {
+		int lo = 0;
+		int hi = list.size() - 1;
+
+		while (lo <= hi) {
+			int m = (lo + hi) / 2;
+			if (list.get(m) < n) {
+				lo = m + 1;
+			} else {
+				hi = m - 1;
+			}
+
+		}
+		return lo;
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for(int n : arr) {
+			int idx = getIdx(n);
+			if(idx == list.size()) {
+				list.add(n);
+			}else {
+				list.set(idx,n);
+			}
+		}
+		System.out.println(list.size());
+
+	}
+
+}
