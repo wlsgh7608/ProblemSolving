288204e09b213194e073eeb758031eaa01830beb
wlsgh7608 <62232531+wlsgh7608@users.noreply.github.com>
2022-10-23 15:43:24 +0900

    [Gold II] Title: 반도체 설계, Time: 296 ms, Memory: 21084 KB -BaekjoonHub

---
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static List<Integer> lis = new ArrayList<>();
+
+	static int getIdx(int target) {
+		int lo = 0;
+		int hi =  lis.size()-1;
+		while(lo<=hi) {
+			int m = (lo+hi)/2;
+			
+			if(lis.get(m)<target) {
+				lo = m+1;
+			}else {
+				hi = m-1;
+			}
+		}
+		return lo;
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for(int n:arr) {
+			int idx = getIdx(n);
+			if(idx==lis.size()) {
+				lis.add(n);
+			}else {
+				lis.set(idx, n);
+			}
+		}
+		System.out.println(lis.size());
+		
+
+	}
+
+}
