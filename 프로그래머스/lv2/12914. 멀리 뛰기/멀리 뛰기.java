12cb51a393aca85a202ebddcad4e662432603015
wlsgh7608 <62232531+wlsgh7608@users.noreply.github.com>
2022-10-29 21:14:02 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.10 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long solution(int n) {
+        int[] dp  = new int[n+1];
+        dp[1] = 1;
+        dp[0] = 1;
+        for(int i =2;i<n+1;i++){
+            dp[i] = (dp[i-2] +dp[i-1])%1234567;
+        }
+        
+        return dp[n];
+    }
+}
