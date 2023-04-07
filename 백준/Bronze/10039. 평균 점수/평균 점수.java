f5997f45a7cbd5d214e334ebd00a089541e57b31
wlsgh7608 <62232531+wlsgh7608@users.noreply.github.com>
2023-04-08 01:04:08 +0900

    [Bronze IV] Title: 평균 점수, Time: 120 ms, Memory: 14076 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int sum= 0;
+		for(int i = 0; i<5;i++) {
+			int score = Integer.parseInt(br.readLine()); 
+			if(score<40) {
+				score = 40;
+			}
+			sum+=score;
+		}
+		System.out.println(sum/5);
+	}
+
+}
