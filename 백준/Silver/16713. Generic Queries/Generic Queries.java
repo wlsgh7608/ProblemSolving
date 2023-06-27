46476bb0e56e7c0ffe9771a231e73d035bb6f039
wlsgh7608 <62232531+wlsgh7608@users.noreply.github.com>
2023-06-27 21:42:16 +0900

    [Silver II] Title: Generic Queries, Time: 1328 ms, Memory: 307268 KB -BaekjoonHub

---
+++ 백준/Silver/16713. Generic Queries/Generic Queries.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int Q = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int[] xors = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            xors[i] = arr[i] ^ xors[i - 1];
+        }
+        int result = 0;
+        for (int i = 0; i < Q; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            result = result ^ (xors[b ] ^ xors[a-1]);
+        }
+        System.out.println(result);
+    }
+
+}
