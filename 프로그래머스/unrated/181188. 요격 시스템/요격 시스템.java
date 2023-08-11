357f026c1cfb5462533e7c93743af153783fe0e5
wlsgh7608 <62232531+wlsgh7608@users.noreply.github.com>
2023-08-11 16:27:14 +0900

    [unrated] Title: 요격 시스템, Time: 0.45 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] targets) {
+        Comparator<int[]> TargetComparator = new Comparator<int[]>(){
+            @Override
+            public int compare(int[] o1,int[] o2){
+                if(o1[1]==o2[1]){
+                    return o1[0]-o2[0];
+                }
+                return o1[1]-o2[1];
+            }
+        };
+        Arrays.sort(targets,TargetComparator);
+        
+        int end = -1;
+        int cnt = 0;
+        for(int[] target: targets){
+            int targetStart = target[0];
+            int targetEnd = target[1];
+        
+            if(targetStart>=end){
+                end = targetEnd;
+                cnt++;
+            }
+        
+        }
+        
+        return cnt;
+    }
+}
