80cd296a8a2049561e487b9943f3e51bf9e51de8
wlsgh7608 <62232531+wlsgh7608@users.noreply.github.com>
2023-08-11 17:40:30 +0900

    [Medium] Time: 0 ms (100.00%), Memory: 41.8 MB (12.03%) - LeetHub

---
+++ LeetCode/Medium/0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        // nums 길이 1000
+        // O(logN)
+        // 양옆보다 큰 index 찾기
+        
+        // O(N)
+        int N = nums.length;
+
+        if(N==1){
+            return 0;
+        }
+        if(nums[0]>nums[1]){
+            return 0;
+        }
+        
+        if( nums[N-1]>nums[N-2]){
+            return N-1;
+        }
+           
+        
+        int lo = 1;
+        int hi = nums.length-2;
+        
+        int answer = 0;
+        while(lo<=hi){
+            int m = (lo+hi)/2;
+            if(nums[m]>nums[m-1]&& nums[m]>nums[m+1]){
+                answer = m;
+                break;
+            }
+            else if(nums[m]<nums[m-1]){
+                hi = m-1;
+            }else if(nums[m]<nums[m+1]){
+                lo = m+1;
+            }
+            
+        }
+        
+        return answer;
+    }
+}
