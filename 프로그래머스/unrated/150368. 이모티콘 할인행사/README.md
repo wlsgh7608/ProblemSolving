5a7027a0a5045bd3aa4d4c1dc95dc268266d96c4
wlsgh7608 <62232531+wlsgh7608@users.noreply.github.com>
2023-10-07 16:36:54 +0900

    [unrated] Title: 이모티콘 할인행사, Time: 0.03 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/unrated/150368. 이모티콘 할인행사/README.md
+++ 프로그래머스/unrated/150368. 이모티콘 할인행사/README.md
@@ -1,10 +1,10 @@
 # [unrated] 이모티콘 할인행사 - 150368 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150368?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150368) 
 
 ### 성능 요약
 
-메모리: 76 MB, 시간: 0.03 ms
+메모리: 83.5 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-Empty
+정확성: 100.0<br/>합계: 100.0 / 100.0
 
 ### 문제 설명
 
--- 프로그래머스/unrated/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ 프로그래머스/unrated/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -1,81 +1,70 @@
-import java.util.*;
 class Solution {
     
-    
-    static int[] simulate(int[][] users, int[] emoticons){
-        int cnt =0;
-        int tot = 0;
+    static int[] process(int[][] users,int[] emoticons){
         
-        // 유저마다 이모티콘 구매 확인
+        int totPrice = 0;
+        int plus = 0;
         for(int[] user : users){
-            int price = 0;
-            int buyRate = user[0];
-            int register = user[1];
             
             
+            int tot = 0;
             for(int i = 0 ; i<N;i++){
-                // 유저가 정한 할인율 보다 높은 것이 있다면
-                // 이모티콘 구매
-                if(emotiPercent[i] >=buyRate){
-                    price+= emoticons[i]*0.01*(100-emotiPercent[i]);
+                if(arr[i]>=user[0]){
+                    int price = emoticons[i];
+                    
+                    tot+= price*(100-arr[i])*0.01;
                 }
             }
-            // 유저가 이모티콘플러스를 구입하는지 체크
-            if(price>=register){
-                cnt++;
+            
+            if(tot>=user[1]){
+                plus++;
             }else{
-                // 이모티콘 플러스 구입 X
-                tot+=price;
+                totPrice+=tot;
             }
             
         }
         
-        return new int[]{cnt,tot};
+        return new int[]{plus,totPrice};
+        
     }
     
-    
-    static void dfs(int idx,int[][] users,int[] emoticons){
-        if(idx==N){
-            // 시뮬레이션 시작
-            
-            int[] result = simulate(users,emoticons);
-            int cnt = result[0];
-            int tot = result[1];
-            if(maxCnt<cnt){
-                maxCnt = cnt;
-                maxSale = tot;
-            }else if(maxCnt==cnt){
-                if(maxSale<tot){
-                    maxSale = tot;
-                }
+    static void dfs(int idx,int[][] users,int[] emoticons ){
+        if(idx == N){
+            int[] result = process(users,emoticons);
+            if(result[0]>maxPlus){
+                maxPlus = result[0];
+                maxPrice = result[1];
+            }else if(result[0] == maxPlus && result[1]>maxPrice){
+                maxPrice = result[1];
             }
             return;
+            // 수행
         }
         
-        for(int i = 0 ; i<4;i++){
-            // 이모티콘의 할인율 할당
-            emotiPercent[idx] = salePercent[i];
+        for(int i= 0  ; i<4;i++){
+            arr[idx] = percent[i];
             dfs(idx+1,users,emoticons);
         }
+        
+        
     }
     
     
-    static int[] salePercent = {10,20,30,40};
-    static int[] emotiPercent;
-    static int maxCnt;
-    static int maxSale;
+    static int[] percent = {10,20,30,40};
+    static int[] arr;
+    
     static int N;
+    static int maxPlus;
+    static int maxPrice;
+    
     
     public int[] solution(int[][] users, int[] emoticons) {
-        N= emoticons.length;
-        emotiPercent = new int[N];
+        int[] answer = {};
+        N = emoticons.length;
+        arr = new int[N];
         
-        // 이모티콘의 크기는 최대 7이다 
-        // 브루트포스가 가능하다 => 4^7= 2^14
         dfs(0,users,emoticons);
-        
-        return new int[]{maxCnt,maxSale};
-        
+        return new int[]{maxPlus,maxPrice};
         
     }
 }
