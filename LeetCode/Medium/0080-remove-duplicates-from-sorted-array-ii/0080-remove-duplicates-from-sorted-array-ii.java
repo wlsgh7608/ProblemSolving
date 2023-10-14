f09950699c5882dec27531057e9f819923bd57c4
wlsgh7608 <62232531+wlsgh7608@users.noreply.github.com>
2023-10-14 11:56:47 +0900

    [Medium] Time: 0 ms (100.00%), Memory: 44 MB (13.54%) - LeetHub

---
+++ LeetCode/Medium/0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        boolean isSame = false;
+        int prev = -100_000;
+        
+        int p = 0;
+        for(int i = 0; i < nums.length;i++){
+            int n = nums[i];
+            if(prev!=n){
+                prev = n;
+                nums[p++] = n;
+                isSame = false;
+            }else if(prev==n){
+                if(!isSame){
+                    nums[p++] = n;
+                
+                    isSame = true;
+                }else{
+                    continue;
+                }
+            }
+        }
+        return p;
+        
+    }
+}
