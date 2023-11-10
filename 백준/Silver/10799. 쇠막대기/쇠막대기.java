58ac924904b2a655b5600b393ef5b2a877f639fe
wlsgh7608 <62232531+wlsgh7608@users.noreply.github.com>
2023-11-10 22:35:30 +0900

    [Silver II] Title: 쇠막대기, Time: 160 ms, Memory: 15676 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -1,28 +1,27 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
-        Stack<Character> S = new Stack<>();
 
-        long cnt = 0;
+        int stackCnt = 0;
+        int cnt = 0;
         char prev = ')';
 
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
             if (c == ')') {
                 // lazer
-                S.pop();
+                stackCnt--;
                 if (prev == '(') {
-                    cnt += S.size();
+                    cnt += stackCnt;
                 } else {
                     cnt += 1;
                 }
             } else {
-                S.add('(');
+                stackCnt++;
             }
             prev = c;
         }
