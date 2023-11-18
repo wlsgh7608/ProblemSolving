90f6745aa68dc8c4ae9477634b3a3330151bb3e4
wlsgh7608 <62232531+wlsgh7608@users.noreply.github.com>
2023-11-18 09:30:40 +0900

    [Gold V] Title: 입국심사, Time: 340 ms, Memory: 23752 KB -BaekjoonHub

--- 백준/Gold/3079. 입국심사/입국심사.java
+++ 백준/Gold/3079. 입국심사/입국심사.java
@@ -3,40 +3,43 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		long[] judges = new long[N];
-		for (int i = 0; i < N; i++) {
-			judges[i] = Long.parseLong(br.readLine());
-		}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long N = Long.parseLong(st.nextToken());
+        long M = Long.parseLong(st.nextToken());
+        long[] judges = new long[(int) N];
+        for (int i = 0; i < N; i++) {
+            judges[i] = Long.parseLong(br.readLine());
+        }
 
-		long max = 0;
-		for (long n : judges) {
-			max = Math.max(max, n);
-		}
+        long max = 0;
+        for (long n : judges) {
+            max = Math.max(max, n);
+        }
 
-		long lo = 1;
-		long hi = max * M;
+        long lo = 0;
+        long hi = max * 1_000_000_000L;
 
-		while (lo <= hi) {
-			long m = (lo + hi) / 2;
+        while (lo <= hi) {
+            long m = (lo + hi) / 2;
 
-			long canJudge = 0;
-			for (long n : judges) {
-				canJudge += m / n;
-			}
-			if (canJudge >= M) {
-				hi = m - 1;
-			} else {
-				lo = m + 1;
-			}
+            long canJudge = 0;
+            for (long n : judges) {
+                canJudge += m / n;
+                if (canJudge >= M) {
+                    hi = m - 1;
+                    break;
+                }
+            }
+            if (canJudge >= M) {
+                hi = m - 1;
+            } else {
+                lo = m + 1;
+            }
 
-		}
-		System.out.println(lo);
+        }
+        System.out.println(lo);
 
-	}
-
+    }
 }
