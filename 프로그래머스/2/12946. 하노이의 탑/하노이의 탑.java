73c4529920c3bb912a1321f6e3a8abb440e082de
wlsgh7608 <62232531+wlsgh7608@users.noreply.github.com>
2023-12-02 14:02:34 +0900

    [level 2] Title: 하노이의 탑, Time: 4.93 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -1,17 +1,16 @@
 import java.util.*;
 class Solution {
-    static List<int[]> routes;
-    static void move(int start,int end,int other, int size){
+    static void move(int start,int end,int other, int size,List<int[]> routes){
         if(size==0){
             return;
         }
-        move(start,other,end,size-1);
+        move(start,other,end,size-1,routes);
         routes.add(new int[]{start,end});
-        move(other,end,start,size-1);
+        move(other,end,start,size-1,routes);
     }
     public int[][] solution(int n) {
-        routes = new ArrayList<>();
-        move(1,3,2,n);
+        List<int[]> routes = new ArrayList<>();
+        move(1,3,2,n,routes);
         int N = routes.size();
         int[][] answer = new int[N][2];
         for(int i = 0; i < N; i++){
