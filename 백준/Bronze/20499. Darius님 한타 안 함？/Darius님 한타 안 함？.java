fee31d04c648aebc18cf1c16c3a6acc65124605a
wlsgh7608 <62232531+wlsgh7608@users.noreply.github.com>
2023-12-25 23:11:49 +0900

    [Bronze IV] Title: Darius님 한타 안 함?, Time: 124 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/20499. Darius님 한타 안 함？/Darius님 한타 안 함？.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), "/");
+        int K = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+        int A = Integer.parseInt(st.nextToken());
+
+        if (K + A < D || D == 0) {
+            System.out.println("hasu");
+
+        } else {
+            System.out.println("gosu");
+        }
+    }
+}
