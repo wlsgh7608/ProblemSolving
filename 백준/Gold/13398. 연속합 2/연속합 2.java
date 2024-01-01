4a980a2ea9373b423678770a9ee61f6d46993a94
wlsgh7608 <62232531+wlsgh7608@users.noreply.github.com>
2024-01-01 15:41:56 +0900

    [Gold V] Title: 연속합 2, Time: 280 ms, Memory: 25592 KB -BaekjoonHub

---
+++ 백준/Gold/13398. 연속합 2/연속합 2.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[][] dp = new int[N][2];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp[0][0] = arr[0];
+        int result = arr[0];
+
+        for (int i = 1; i < N; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0] + arr[i], arr[i]);
+            dp[i][1] = Math.max(dp[i - 1][0], dp[i - 1][1] + arr[i]);
+
+            int maxValue = Math.max(dp[i][0], dp[i][1]);
+            result = Math.max(result, maxValue);
+        }
+        System.out.println(result);
+
+    }
+}
