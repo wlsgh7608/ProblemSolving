5afb6917e51abd62bd44b6ebfa7e5bbacb94ffed
wlsgh7608 <62232531+wlsgh7608@users.noreply.github.com>
2024-01-04 21:11:09 +0700

    [Bronze V] Title: 웰컴, Time: 120 ms, Memory: 13852 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+
+}
