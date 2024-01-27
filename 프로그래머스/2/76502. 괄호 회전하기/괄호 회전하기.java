a8d2860cb33dbb5b1f0f650c227b53ac946dc7d6
wlsgh7608 <62232531+wlsgh7608@users.noreply.github.com>
2024-01-27 09:30:27 +0900

    [level 2] Title: 괄호 회전하기, Time: 1.34 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int solution(String s) {
+        // 길이 1000
+        // 회전 1000
+        
+        
+        
+        int size = s.length();
+        int cnt = 0;
+        
+        for(int i = 0 ; i<size;i++){
+            //회전 시킨다.
+            String endStr =s.substring(0, i);
+            String startStr = s.substring(i);
+            String str = startStr + endStr;
+            // System.out.println(str);
+            
+            for(int j= 0 ;j <size;j++){
+                str = str.replace("{}","");
+                str = str.replace("[]","");
+                str = str.replace("()","");
+
+            }            
+            if(str.length()==0){
+                cnt++;
+            }
+        }
+
+        return cnt;
+        
+    }
+}
