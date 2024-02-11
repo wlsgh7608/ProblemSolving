add05204a6cf0adf969c1205d9f7297affe55083
wlsgh7608 <62232531+wlsgh7608@users.noreply.github.com>
2024-02-11 17:51:49 +0900

    [level 3] Title: 등굣길, Time: 0.84 ms, Memory: 52.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -0,0 +1,32 @@
+class Solution {
+    public final int MOD = 1_000_000_007;
+
+    public int solution(int m, int n, int[][] puddles) {
+        // (1,1) -> (m,n)
+        int[][] dp = new int[m + 1][n + 1];
+
+        // 웅덩이의 dp 값은 -1로 한다.
+        for (int[] puddle : puddles) {
+            int x = puddle[0];
+            int y = puddle[1];
+            dp[x][y] = -1;
+        }
+
+        dp[1][1] = 1;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (dp[i][j] == -1) {
+                    continue;
+                }
+                if(dp[i-1][j]!=-1){
+                    dp[i][j]+= dp[i-1][j];
+                }
+                if(dp[i][j-1]!=-1){
+                    dp[i][j] += dp[i][j-1];
+                }
+                dp[i][j] %= MOD;
+            }
+        }
+        return dp[m][n];
+    }
+}
