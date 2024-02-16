258850c3941f1752db31cff775cdf59c9889ca80
wlsgh7608 <62232531+wlsgh7608@users.noreply.github.com>
2024-02-17 01:31:39 +0900

    [Medium] Time: 0 ms (100.00%), Memory: 41.8 MB (86.95%) - LeetHub

--- LeetCode/Medium/0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ LeetCode/Medium/0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -1,39 +1,27 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
 class Solution {
-    void preOrder(TreeNode cur,Stack<TreeNode> S){
-        if(cur==null){
+
+    void preOrder(TreeNode cur, Stack<TreeNode> S) {
+        if (cur == null) {
             return;
         }
         S.add(cur);
-        preOrder(cur.left,S);
-        preOrder(cur.right,S);
-        
+        preOrder(cur.left, S);
+        preOrder(cur.right, S);
+
     }
+
+    private TreeNode prev = null;
+
     public void flatten(TreeNode root) {
-        Stack<TreeNode> S= new Stack<>();
-        preOrder(root,S);
-        
-        TreeNode prev = null;
-        while(!S.isEmpty()){
-            TreeNode cur = S.pop();
-            cur.left = null;
-            cur.right = prev;
-            prev = cur;
+        if(root == null){
+            return;
         }
-        
+        flatten(root.right);
+        flatten(root.left);
+        root.right = prev;
+        root.left = null;
+        prev = root;
+
     }
 }
