da079605ba0e9b040aa486a35bd7be8265a87428
wlsgh7608 <62232531+wlsgh7608@users.noreply.github.com>
2024-02-27 16:41:15 +0900

    [level 1] Title: 체육복, Time: 0.23 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+
+        HashSet<Integer> lostSet = new HashSet<>();
+        HashSet<Integer> reserveSet = new HashSet<>();
+        for (int l : lost) {
+            lostSet.add(l);
+        }
+        for (int r : reserve) {
+            reserveSet.add(r);
+        }
+
+        for(int l : lost){
+            // 여분을 가져온 학생이 잃어버렸다면
+            if(reserveSet.contains(l)){
+                lostSet.remove(l);
+                reserveSet.remove(l);
+            }
+        }
+
+
+        List<Integer> lostList = new ArrayList<>(lostSet);
+        Collections.sort(lostList);
+        for(int l : lostList){
+            // 왼쪽 학생 확인
+            if (reserveSet.contains(l - 1)) {
+                lostSet.remove(l);
+                reserveSet.remove(l - 1);
+            // 오른쪽 학생 확인
+            } else if (reserveSet.contains(l + 1)) {
+                lostSet.remove(l);
+                reserveSet.remove(l + 1);
+            }
+        }
+        return n- lostSet.size();
+    }
+}
