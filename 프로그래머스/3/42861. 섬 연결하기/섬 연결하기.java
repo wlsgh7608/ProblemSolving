a757c45fa0bb8c7daa9ed6182c5bc765680b521d
wlsgh7608 <62232531+wlsgh7608@users.noreply.github.com>
2024-03-04 22:47:07 +0900

    [level 3] Title: 섬 연결하기, Time: 1.21 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,71 +1,57 @@
 import java.util.*;
+
 class Solution {
-    class Bridge implements Comparable<Bridge> {
-        int v1;
-        int v2;
-        int cost;
-
-        public Bridge(int v1, int v2, int cost) {
-            this.v1 = v1;
-            this.v2 = v2;
-            this.cost = cost;
+    List<Edge>[] edgeList;
+    public void init(int n, int[][] costs){
+        edgeList = new LinkedList[n];
+        for(int i = 0; i < n ; i++){
+            edgeList[i] = new LinkedList<>();
         }
 
-        @Override
-        public int compareTo(Bridge o) {
-            return Integer.compare(this.cost, o.cost);
+        for(int[] cost : costs){
+            Edge edge1 = new Edge(cost[0], cost[2]);
+            Edge edge2 = new Edge(cost[1], cost[2]);
+            edgeList[cost[1]].add(edge1);
+            edgeList[cost[0]].add(edge2);
         }
     }
-
-
-    int[] parent;
-    List<Bridge> bridges = new ArrayList<>();
-
-    void init(int n, int[][] costs) {
-        parent = new int[n];
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
-        }
-
-        for (int[] cost : costs) {
-            bridges.add(new Bridge(cost[0], cost[1], cost[2]));
-        }
-    }
-
-    int getParent(int v) {
-        if (parent[v] == v) {
-            return v;
-        }
-        return parent[v] = getParent(parent[v]);
-    }
-
-
     public int solution(int n, int[][] costs) {
+        int answer = 0;
+        // MST
+        // 다익스트라
         init(n, costs);
 
-        int answer = 0;
-        int edgeCnt = 0;
-        Collections.sort(bridges);
-        for (Bridge bridge : bridges) {
-            if (edgeCnt == n - 1) {
+        // 0부터 시작
+        Queue<Integer> q = new LinkedList<>();
+        q.add(0);
+        boolean[] visited = new boolean[n];
+        visited[0] = true;
+        PriorityQueue<Edge> pq = new PriorityQueue<>((e1, e2) -> e1.cost - e2.cost);
+        int nodeCount = 1;
+        while(!q.isEmpty() && nodeCount < n){
+            int now = q.poll();
+            for(Edge e : edgeList[now]){
+                pq.add(e);
+            }
+            while(!pq.isEmpty()){
+                Edge e = pq.poll(); // 현재 방문한 노드에 연결되어 있으며 가장 비용이 적은 edge
+                if(visited[e.to]) continue; // 방문한 노드라면 = 이미 최단거리를 찾았다면 넘어간다.
+                visited[e.to] = true;
+                answer += e.cost;
+                q.add(e.to);
+                nodeCount++;
                 break;
             }
-
-            int v1 = bridge.v1;
-            int v2 = bridge.v2;
-            int cost = bridge.cost;
-
-            int v1Parent = getParent(v1);
-            int v2Parent = getParent(v2);
-
-            if (v1Parent != v2Parent) {
-                parent[v1Parent] = v2Parent;
-
-                answer += cost;
-                edgeCnt++;
-            }
         }
+
         return answer;
     }
-
+}
+class Edge {
+    int to;
+    int cost;
+    public Edge (int to, int cost){
+        this.to = to;
+        this.cost = cost;
+    }
 }
