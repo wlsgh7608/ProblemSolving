1473c9d189f4fc5b92fdf8bfe6f65acb5f77a018
wlsgh7608 <62232531+wlsgh7608@users.noreply.github.com>
2024-03-14 08:11:40 +0900

    [Silver III] Title: 대표 자연수, Time: 212 ms, Memory: 16764 KB -BaekjoonHub

---
+++ 백준/Silver/2548. 대표 자연수/대표 자연수.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        int minIdx = arr[N / 2];
+        if (N % 2 == 0) {
+            minIdx = arr[N / 2 - 1];
+        }
+
+        System.out.println(minIdx);
+
+    }
+}
