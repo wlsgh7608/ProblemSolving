12689b048eef69cc5b714855fc8dab7bca8df0a9
wlsgh7608 <62232531+wlsgh7608@users.noreply.github.com>
2024-03-16 08:54:01 +0900

    [level 2] Title: 스킬트리, Time: 0.03 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/README.md
+++ 프로그래머스/2/49993. 스킬트리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 스킬트리 - 49993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49993?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49993) 
 
 ### 성능 요약
 
-메모리: 76.4 MB, 시간: 0.03 ms
+메모리: 75.1 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 16일 08:46:35
+2024년 03월 16일 08:54:00
 
 ### 문제 설명
 
--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,15 +1,17 @@
 class Solution {
     static final int ALPHABET = 26;
-
+    
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
+        int[] skillOrder = new int[ALPHABET];
 
-        int[] in = new int[ALPHABET];
 
+        // 스킬트리 순서를 저장
+        // ex) "CBD" => C:1, B:2, D:3
         for (int i = 0; i < skill.length(); i++) {
-            in[skill.charAt(i) - 'A'] = i + 1;
+            int idx = skill.charAt(i) - 'A';
+            skillOrder[idx] = i + 1;
         }
-
 
         fail:
         for (String skill_tree : skill_trees) {
@@ -17,12 +19,16 @@ class Solution {
             int skillLen = skill_tree.length();
             for (int i = 0; i < skillLen; i++) {
                 int idx = skill_tree.charAt(i) - 'A';
-                if (in[idx] == skillDepth) {
+
+                // 선행조건에 없는 스킬은 무시
+                // 선행조건에 있는 스킬이 순서에 맞지 않으면 실패
+                if (skillOrder[idx] == skillDepth) {
                     skillDepth++;
-                } else if (in[idx] > skillDepth) {
+                } else if (skillOrder[idx] > skillDepth) {
                     continue fail;
                 }
             }
+            // 주어진 스킬트리가 선행조건에 만족하면  성공
             answer++;
         }
         return answer;
