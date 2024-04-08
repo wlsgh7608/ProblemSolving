83363fd7bc433189442099fddf54eab7dd796fd1
wlsgh7608 <62232531+wlsgh7608@users.noreply.github.com>
2024-04-08 23:06:49 +0900

    [level 3] Title: 양과 늑대, Time: 0.71 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,43 +1,56 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+class Solution {
+        static final int ROOT = 0, SHEEP = 0, WOLF = 1;
+        int N, answer = 0;
+        boolean[] visited;
+        List<Integer>[] graph;
 
-public class Solution {
+        public int solution(int[] info, int[][] edges) {
+            init(info, edges);
 
+            visit(ROOT, 0, 0, info);
 
-    int answer = 0;
-    boolean[] visited;
-
-    private void dfs(int sheep, int wolf, int[] info, int[][] edges) {
-        if (sheep <= wolf) {
-            return;
+            return answer;
         }
 
-        answer = Math.max(answer, sheep);
+        void init(int[] info, int[][] edges) {
+            N = info.length;
 
-        for (int[] edge : edges) {
-            int from = edge[0];
-            int to = edge[1];
-            if (visited[from] && !visited[to]) {
-                visited[to] = true;
-                if (info[to] == 0) {
-                    dfs(sheep + 1, wolf, info, edges);
-                } else {
-                    dfs(sheep, wolf + 1, info, edges);
-                }
-                visited[to] = false;
+            visited = new boolean[N];
+            graph = new ArrayList[N];
+            for (int i=0; i<N; i++) {
+                graph[i] = new ArrayList<>();
+            }
+
+            for (int[] edge : edges) {
+                int a = edge[0];
+                int b = edge[1];
+
+                graph[a].add(b);
             }
         }
+
+        void visit(int now, int sheep, int wolf, int[] info) {
+            // if (visited[now]) return;
+
+
+            if(info[now] == SHEEP) sheep++;
+            else wolf++;
+
+            if (sheep <= wolf) {
+                return;
+            }
+            
+            visited[now] = true;
+            answer = Math.max(answer, sheep);
+
+            for (int i=0; i<N; i++) {
+                if(!visited[i]) continue;
+                for (int next : graph[i]) {
+                    if(!visited[next]) visit(next, sheep, wolf, info);
+                }
+            }
+
+            visited[now] = false;
+        }
     }
-
-    public int solution(int[] info, int[][] edges) {
-        int N = info.length;
-
-        visited = new boolean[N];
-        visited[0] = true;
-
-        dfs(1, 0, info, edges);
-        
-
-        return answer;
-    }
-}
