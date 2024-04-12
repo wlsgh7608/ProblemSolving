3dfc3bff01d5404ed2cf49830b12db47710d0d9b
wlsgh7608 <62232531+wlsgh7608@users.noreply.github.com>
2024-04-12 18:48:21 +0900

    [Silver I] Title: NM과 K (1), Time: 2904 ms, Memory: 297320 KB -BaekjoonHub

---
+++ 백준/Silver/18290. NM과 K （1）/NM과 K （1）.java
@@ -0,0 +1,81 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M, K;
+    static int answer = -40_000;
+    static int[][] visited;
+    static int[][] G;
+
+
+    static void visitCheck(int x, int y, int value) {
+        int[] dx = {-1, 1, 0, 0, 0};
+        int[] dy = {0, 0, -1, 1, 0};
+
+
+        for (int i = 0; i < 5; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
+                continue;
+            }
+
+            visited[nx][ny] += value;
+
+        }
+    }
+
+    static void dfs(int depth, int sum) {
+
+        //        System.out.println(depth+" "+sum);
+        if (depth == K) {
+            answer = Math.max(answer, sum);
+            return;
+        }
+
+//        for(boolean[] row: visited){
+//            System.out.println(Arrays.toString(row));
+//        }
+
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (visited[i][j] > 0) continue;
+                visitCheck(i, j, 1);
+
+                dfs(depth + 1, sum + G[i][j]);
+                visitCheck(i, j, -1);
+            }
+        }
+
+
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        G = new int[N][M];
+        visited = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                G[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dfs(0, 0);
+        System.out.println(answer);
+
+
+    }
+}
