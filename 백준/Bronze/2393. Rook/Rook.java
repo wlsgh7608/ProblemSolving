e85b19c432ba991a5916bbba21a29d5798cd6953
wlsgh7608 <62232531+wlsgh7608@users.noreply.github.com>
2024-11-23 00:09:41 +0900

    [Bronze V] Title: Rook, Time: 96 ms, Memory: 14008 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        StringBuilder sb = new StringBuilder();
+        sb.append("  ___  ___  ___").append("\n");
+        sb.append("  | |__| |__| |").append("\n");
+        sb.append("  |           |").append("\n");
+        sb.append("   \\_________/").append("\n");
+        sb.append("    \\_______/").append("\n");
+        sb.append("     |     |").append("\n");
+        sb.append("     |     |").append("\n");
+        sb.append("     |     |").append("\n");
+        sb.append("     |     |").append("\n");
+        sb.append("     |_____|").append("\n");
+        sb.append("  __/       \\__").append("\n");
+        sb.append(" /             \\").append("\n");
+        sb.append("/_______________\\").append("\n");
+
+        System.out.println(sb);
+    }
+}
