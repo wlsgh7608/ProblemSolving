321dd7ba117514f21fd0d9084ccb2073efb5ddff
wlsgh7608 <62232531+wlsgh7608@users.noreply.github.com>
2024-11-25 23:57:26 +0900

    [Bronze V] Title: 余り (Remainder), Time: 100 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Bronze/24078. 余り （Remainder）/余り （Remainder）.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int remainder = N % 21;
+        System.out.println(remainder);
+
+    }
+}
