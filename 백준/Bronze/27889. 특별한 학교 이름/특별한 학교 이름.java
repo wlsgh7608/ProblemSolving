2b1c37aef9cd39efd6516e1e35ad20300e14716f
wlsgh7608 <62232531+wlsgh7608@users.noreply.github.com>
2024-12-05 23:24:27 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 96 ms, Memory: 14072 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashMap<String, String> hm = new HashMap<>();
+        hm.put("NLCS","North London Collegiate School");
+        hm.put("BHA","Branksome Hall Asia");
+        hm.put("KIS","Korea International School");
+        hm.put("SJA","St. Johnsbury Academy");
+
+        String input = br.readLine();
+        System.out.println(hm.get(input));
+
+    }
+}
